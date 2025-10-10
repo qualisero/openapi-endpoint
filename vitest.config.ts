89a528382818240c6f5dc56fd96dd2ecec661f1d
Dave dV <dave@tenko.ai>
2025-10-10 10:30:42 +0900

    fixing alias issue for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
   test: {
@@ -15,5 +16,10 @@ export default defineConfig({
     clearMocks: true,
     mockReset: true,
     restoreMocks: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
